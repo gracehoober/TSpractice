a462b30ba4b698646c0bac4bff83f32b9162fd3f
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-02-08 14:54:38 -0800

    explicitly writing out types for the functions

--- ts-loan-calc/calculator.ts
+++ ts-loan-calc/calculator.ts
@@ -7,24 +7,26 @@ const rateInput = document.getElementById("loan-rate") as HTMLInputElement;
 const resultArea = document.getElementById("calc-monthly-payment") as HTMLSpanElement;
 
 const resultHistory: object[] = [];
-
 /** Retrieve form values.
  *
  * Example output: an object like {"amount": 10000, "years": 10, "rate": 4.5}.
  *
  * */
 
-function getFormValues(): object {
+function getFormValues(): { amount: number; years: number; rate: number; } {
   return {
     amount: Number(amountInput.value),
     years: Number(yearsInput.value),
     rate: Number(rateInput.value),
   };
+
 }
 
 /** Calculate monthly payment and return. */
 
-function calcMonthlyPayment({ amount, years, rate }):number {
+function calcMonthlyPayment(
+  { amount, years, rate }:{ amount: number; years: number; rate: number; }
+  ): number {
   const monthsInYear = 12;
   const monthlyRate = (rate / 100) / monthsInYear;
   const n = Math.floor(years * monthsInYear);
@@ -36,8 +38,8 @@ function calcMonthlyPayment({ amount, years, rate }):number {
 
 /** Get form values, calculate, format to 2 decimal places, and display. */
 
-function getFormValuesAndDisplayResults() {
-  const { amount, years, rate } = getFormValues();
+function getFormValuesAndDisplayResults(): void {
+  const { amount, years, rate }: { amount: number; years: number; rate: number; } = getFormValues();
   const payment = calcMonthlyPayment({ amount, years, rate });
   resultHistory.push({ amount, years, rate, payment });
   resultArea.innerText = "$" + payment.toFixed(2);
@@ -45,7 +47,7 @@ function getFormValuesAndDisplayResults() {
 
 /** Set initial form values and show initial results. Called at app start. */
 
-function setInitialValues() {
+function setInitialValues(): void {
   amountInput.value = "10000";
   yearsInput.value = "10";
   rateInput.value = "4.5";
@@ -54,10 +56,10 @@ function setInitialValues() {
 
 /** Start: set form defaults & display; attach form submit event listener. */
 
-function start() {
+function start(): void {
   setInitialValues();
 
-  calcForm.addEventListener("submit", function (evt) {
+  calcForm.addEventListener("submit", function (evt): void {
     evt.preventDefault();
     getFormValuesAndDisplayResults();
   });
